e2f2fdced1407d3c1eb565961ec04c0e6e5192fd
k1ngbau <rartuyo@gmail.com>
2020-05-17 23:02:15 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Branch 2
                 </div>
 
                 <div class="links">
