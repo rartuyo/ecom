b29db00acfc00aa70545fce50a077c34ab0eb540
Rodem <roden.artuyo@sun-asterisk.com>
2020-07-14 22:31:54 +0800

    Change Branch to Artuyo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Asuka Branch
+                    Laravel Asuka Artuyo
 
                 </div>
 
